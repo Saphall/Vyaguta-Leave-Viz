dca9726c3ec83a3da168de506f345717447f11b2
Saphall <safalsakha@gmail.com>
2024-09-22 18:20:12 +0545

    chore: format code

--- backend/utils/util.py
+++ backend/utils/util.py
@@ -1,6 +1,5 @@
 #  Utility functions for the backend
 
-
 def convert_list_to_dict(cursor):
     # Convert the list of tuples to a list of dictionaries
     columns = [desc[0] for desc in cursor.description]
